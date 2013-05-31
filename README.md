8c4986ff880dd26e9a86a4256ba88bba6227f0b2
Sebastian Rosengren <sebastian@rosengren.me>
2013-05-31 22:39:16 +0200

    Implemented functionality and wrote readme

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 Responsy.js
 ===========
 
-A simple tool for firing callbacks when the browsers width changes
+A simple tool for firing callbacks when the browsers width changes.
+
+Usage
+-----------
+Simply drop in responsy.js in your html and attach callbacks to the breakpoints that you need.
+Responsy.js exposes the following functions:
+
+**addBreakpoint(brekpoint)**
+
+Takes a breakpoint object in the following format.
+
+*name:* A unique string identifier. Required.
+
+*lowerBound:* Integer that indicates the lower part of the breakpoint, Optional.
+
+*upperBound:* Integer that indicates the upper part of the breakpoint, Optional.
+
+*callbacks:* An object with named functions that gets called when the browser width is within the breakpoints upper and lower bounds.
+
+**addCallback(breakpointName, callbackName, callback)**
+
+Adds a callback to an existing breakpoint.
+
+*breakpointName:* String identifier of a existing breakpoint.
+
+*callbackName:* The name of your new callback.
+
+*callback:* a function.
+
+**removeBreakpoint(breakpointName)**
+
+Simply removes a breakpoint from responsy.
+
+**removeCallback(breakpointName, callbackName)**
+
+Removes a named callback from the specified breakpoint.
+
+Defaults
+---------
+
+Responsy have 5 default breakpoints that you can add callbacks to. These correspond to **bootstraps** responsive breakpoints.
+
+* phone <= 480
+* phoneTablet <= 767
+* tablet 768 - 979
+* desktop 980 - 1199
+* large >= 1200
+
+Examples
+---------
+
+Add a new callback named output to the default **large** breakpoint
+
+`responsy.addCallback('large','output',outputFunction);`
---
+++ responsy.js
@@ -0,0 +1,130 @@
+var responsy = (function() {
+        //Private properties and functions
+    var privateObject = (function(privateModule){
+            privateModule = {
+                activeBreakpoints: [],
+                breakpoints: {
+                    phone: {
+                        name: 'phone',
+                        upperBound: 480
+                    },
+                    phoneTablet: {
+                        name: 'phoneTablet',
+                        upperBound: 767
+                    },
+                    tablet: {
+                        name: 'tablet',
+                        lowerBound: 768,
+                        upperBound: 979
+                    },
+                    desktop: {
+                        name: 'desktop',
+                        lowerBound: 980,
+                        upperBound: 1199
+                    },
+                    large: {
+                        name: 'large',
+                        lowerBound: 1200
+                    }
+                },
+                triggerCallbacks: function(breakpoint) {
+                    for (var callback in breakpoint.callbacks) {
+                        if (breakpoint.callbacks.hasOwnProperty(callback)) {
+                            breakpoint.callbacks[callback]();
+                        }
+                    }
+                },
+                addActive: function(breakpoint){
+                    if(!privateModule.isActive(breakpoint)){
+                        privateModule.activeBreakpoints.push(breakpoint.name);
+                    }
+                },
+                removeActive: function(breakpoint){
+                    var i = 0;
+                    for(; i < privateModule.activeBreakpoints.length; i++){
+                        if(privateModule.activeBreakpoints[i] === breakpoint.name){
+                            privateModule.activeBreakpoints.splice(i,1);
+                            break;
+                        }
+                    }
+                },
+                isWithinBounds: function(width, breakpoint) {
+                    var withinBounds = breakpoint.upperBound || breakpoint.lowerBound;
+                    if(breakpoint.upperBound && breakpoint.upperBound <= width){
+                        withinBounds = false;
+                    }
+                    if(breakpoint.lowerBound && breakpoint.lowerBound >= width){
+                        withinBounds = false;
+                    }
+                    return withinBounds;
+                },
+                isActive: function(breakpoint){
+                    for(var i = 0; i < privateModule.activeBreakpoints.length; i++) {
+                        if (breakpoint.name === privateModule.activeBreakpoints[i]){
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            };
+            return privateModule;
+        }({})),
+        //The object that contains all public methods and properties
+        publicObject = (function(publicModule){
+            publicModule = {
+                addBreakpoint: function(breakpoint) {
+                    if (breakpoint && breakpoint.name) {
+                        privateObject.breakpoints[breakpoint.name] = breakpoint;
+                    }
+                },
+                //Adds a named callback to the specified breakpoint
+                addCallback: function(breakpointName, callbackName, callback) {
+                    if(privateObject.breakpoints[breakpointName]){
+                        privateObject.breakpoints[breakpointName].callbacks = privateObject.breakpoints[breakpointName].callbacks || {};
+                        privateObject.breakpoints[breakpointName].callbacks[callbackName] = callback;
+                    }
+                },
+                removeBreakpoint: function(breakpointName) {
+                    delete privateObject[breakpointName];
+                },
+                //Removes a named callback from the specified breakpoint
+                removeCallback: function(breakpointName, callbackName) {
+                    if (privateObject[breakpointName]) {
+                        delete privateObject[breakpointName][callbackName];
+                    }
+                },
+                getActiveBreakpoints: function(){
+                    return privateObject.activeBreakpoints;
+                }
+            };
+            return publicModule;
+        }({})),
+        //The handler that we later attach to window.resize event
+        resizeHandler = function() {
+            var width = window.innerWidth;
+            for (var prop in privateObject.breakpoints) {
+                //Iterate over all breakpoints
+                if (privateObject.breakpoints.hasOwnProperty(prop)) {
+                    (function(breakpoint){
+                        if (!privateObject.isActive(breakpoint) && privateObject.isWithinBounds(width,breakpoint)) {
+                            privateObject.triggerCallbacks(breakpoint);
+                            privateObject.addActive(breakpoint);
+                        }
+                        else if (privateObject.isActive(breakpoint) && !privateObject.isWithinBounds(width,breakpoint)) {
+                            privateObject.removeActive(breakpoint);
+                        }
+                    }(privateObject.breakpoints[prop]));
+
+                }
+            }
+        }
+
+    //Attach eventhandler for the windows resize event
+    if (window.addEventListener) {
+        window.addEventListener('resize', resizeHandler, true);
+    } else if (window.attachEvent) {
+        window.attachEvent('onresize', resizeHandler);
+    }
+
+    return publicObject;
+}());
